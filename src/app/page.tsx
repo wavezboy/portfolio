855d737a8a92faa143ca2927b40fea4566565302
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-28 18:40:01 +0100

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="  min-h-screen px-[170.91px]">
-      <div>Be there</div>
-      <div>okkkkkkkkk</div>
-      <div>hjjshKCGjchcsagj</div>
-      <div>
-        I'm a software engineer based in Toronto, Canada and also a
-        communication and journalism student. I enjoy creating things that live
-        on the internet, whether that be websites, applications, or anything in
-        between. I have been freelancing for a year now while studying at the
-        university and I've manage to gain a decent amount of experience and
-        valuable knowledge from all different kinds of fields throughout my
-        projects/work.
-      </div>
-    </main>
-  );
+  return <main className="  min-h-screen px-[170.91px]"></main>;
 }
