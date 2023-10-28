654c7c17b560f4a773d7ff91e843ab7a443a883a
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-28 19:36:01 +0100

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Navbar() {
-  return <div className="text-gradient">Navbar</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-between mt-[20px] ">
+        <p className=" text-[53px] font-normal">DR.</p>
+        <div className="flex gap-[66.32px] items-center">
+          <span className="block font-Inter text-[40px] font-normal">
+            Projects
+          </span>
+          <span className="block font-Inter text-[40px] font-normal">
+            Resume
+          </span>
+          <span className="block font-Inter text-[40px] font-normal">
+            Contact
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
