419f9005a8265a07a4ccfeea3eca92fdd1dfc955
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-28 18:41:50 +0100

    nvab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="  min-h-screen px-[170.91px]"></main>;
+  return (
+    <main className="  min-h-screen px-[170.91px]">
+      <Navbar />
+    </main>
+  );
 }
