527cd7fca3954c9b1cd6a897f75b1013f4060459
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-28 18:57:45 +0100

    gradient setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        gradient:
+          "linear-gradient(90deg, #9845E8 -1.72%, #33D2FF 54.05%, #DD5789 99.78%)",
       },
     },
     fontFamily: {
-      Inter : "Inter"
-    }
-
-    }
+      Inter: "Inter",
+    },
   },
   plugins: [],
-
-}
-export default config
+};
+export default config;
